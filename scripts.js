b371aeb28f1ba62a5ebf44fe8eda9b90c0def5f5
Alex Roper <alex@esrl.org>
2025-01-13 17:09:16 -0500

    Created add, subtract, multiply, and divide functions.

---
+++ scripts.js
@@ -0,0 +1,21 @@
+function add(num1,num2) {
+    return num1 + num2;
+}
+
+function subract(num1,num2) {
+    return num1 - num2;
+}
+
+function multiply(num1,num2) {
+    return num1 * num2;
+}
+
+function divide(num1,num2) {
+    return num1 / num2;
+}
+
+console.log(add(4,5));
+console.log(subract(4,5));
+console.log(multiply(4,5));
+console.log(divide(4,5));
+
